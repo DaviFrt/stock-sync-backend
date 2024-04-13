47c5ccaba2d780ef75a3d09977658da580920577
DaviFrt <daviamomfb@gmail.com>
2024-04-13 15:34:42 -0300

    fix: fix status code of global zod errors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 app.register(appRoutes)
 
 app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
-    return reply.send(400).send({
+    return reply.status(400).send({
       message: 'Validation error.',
       issues: error.format(),
     })
